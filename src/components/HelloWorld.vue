a072324c3d52ff2c421816bcb4b01219e22ca350
K4keye <vps32@naver.com>
2022-07-02 16:53:49 +0900

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,55 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <canvas id="canvas" style="height: 300px; width: 300px; background-color: yellow"></canvas>
   </div>
 </template>
 
 <script>
+import * as THREE from "three";
+//import { DragControls } from "https://unpkg.com/three@0.108.0/examples/jsm/controls/DragControls.js";
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },created(){
+    console.log("created")
+  },
+  mounted() {
+    console.log("mounted")
+    this.init();
+    this.animate();
+  },
+  methods:{
+    init: function() {
+      this.scene = new THREE.Scene()
+      this.camera = new THREE.PerspectiveCamera(
+          75,
+          window.innerWidth / window.innerHeight,
+          0.1,
+          1000
+      )
+
+      this.renderer = new THREE.WebGLRenderer()
+      this.renderer.setSize(window.innerWidth, window.innerHeight)
+      document.body.appendChild(this.renderer.domElement)
+
+      const geometry = new THREE.BoxGeometry(1, 1, 1)
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ffAA })
+      this.cube = new THREE.Mesh(geometry, material)
+      this.scene.add(this.cube)
+
+      this.camera.position.z = 5
+
+    },
+    animate: function() {
+      requestAnimationFrame(this.animate)
+
+      this.cube.rotation.x += 0.01
+      this.cube.rotation.y += 0.01
+
+      this.renderer.render(this.scene, this.camera)
+    }
+
   }
 }
 </script>
---
+++ src/components/ImageFrame.vue
@@ -0,0 +1,193 @@
+<template>
+  <div >
+  </div>
+</template>
+
+<script>
+import * as THREE from "three";
+//import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+
+export default {
+  name: 'ImageFrame',
+  props: {
+    msg: String
+  }, created() {
+    console.log("created")
+  },
+  mounted() {
+    console.log("mounted")
+    this.init();
+    this.animate();
+
+
+  },
+  data(){
+    return {
+      totalNum :100,
+      WIDTH : 0,
+      HEIGHT : 0,
+      cameraY : 80,
+      cameraZ : 440,
+      isAnimate : false,
+      loadingImage : 0
+
+    }
+  },
+  methods:{
+
+    init(){
+      this.WIDTH = window.innerWidth;
+      this.HEIGHT = window.innerHeight;
+      this.scene = new THREE.Scene();
+      this.scene.background = new THREE.Color("#000000"); //배경 컬러
+      this.camera = new THREE.PerspectiveCamera(75, this.WIDTH / this.HEIGHT, 0.1, 1000);
+      this.camera.position.set(0, this.cameraY, this.cameraZ);
+
+
+
+      this.renderer = new THREE.WebGLRenderer({ antialias: true });
+      this.renderer.setSize(this.WIDTH, this.HEIGHT);
+      this.renderer.shadowMap.enabled = true;
+      //그림자 활성화
+
+      document.body.appendChild(this.renderer.domElement);
+
+      //조명 넣기
+      var light = new THREE.HemisphereLight(0xffffff, 0x080820, 0.8);
+      light.position.set(100, 100, 0);
+      this.scene.add(light);
+
+
+      {
+
+        const geometry = new THREE.BoxGeometry(400, 400, 2);
+        const material = new THREE.MeshPhongMaterial({
+          // map: imageMap,
+          color: 0x464946,
+        });
+        const wallMesh = new THREE.Mesh(geometry, material);
+
+        wallMesh.position.set(0, 0, -1);
+        wallMesh.receiveShadow = true; //default is false
+        // wallMesh.castShadow = true;
+        //this.scene.add(wallMesh);
+      }
+      this.addHomeDoor();
+      for (let i = 0; i <= this.totalNum; i++) {
+        this.addBox(i);
+        // console.log(i);
+      }
+      this.isAnimate=true;
+      this.addLight(15, 15, 20);
+
+      // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+      // this.controls.maxDistance = 440;
+      //
+      // const axes = new THREE.AxesHelper(150);
+      // this.scene.add(axes);
+      //
+      // const gridHelper = new THREE.GridHelper(240, 20,0xFAAFFF,0xAAAAAA);
+      // this.scene.add(gridHelper);
+
+
+
+    },
+    addHomeDoor(){
+      const geometry = new THREE.BoxGeometry(16, 30, 2);
+      const material = new THREE.MeshPhongMaterial({
+         color: 0xFFFFFF,
+      });
+      const cardMesh = new THREE.Mesh(geometry, material);
+      cardMesh.position.set(0, 0, 0); //카드 위치 지정
+      this.scene.add(cardMesh);
+
+      const spotLight = new THREE.SpotLight(0xffffff, 1.2);
+      spotLight.position.set(0, 30, 30);
+      spotLight.angle = Math.PI / 6;
+      spotLight.penumbra = 0.8;
+      spotLight.decay = 1;
+      spotLight.distance = 100;
+      spotLight.castShadow = true;
+      this.scene.add(spotLight);
+      //
+      // const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+      // this.scene.add(spotLightHelper);
+    },
+    addBox(i){
+      const imageMap = new THREE.TextureLoader();
+      imageMap.load("https://source.unsplash.com/collection/" + i, (data) => {
+        const geometry = new THREE.BoxGeometry(16, 12, 1);
+        const material = new THREE.MeshPhongMaterial({
+          map: data,
+          // color: 0xffffff,
+        });
+
+        const cardMesh = new THREE.Mesh(geometry, material);
+        cardMesh.castShadow = true;
+        // cardMesh.receiveShadow = true;
+        let x = Math.random() * 250 - 150; // - 100 ~ 100 까지 랜덤
+        // let y = i * 5;
+        let y = Math.random() * 220 - 70;
+        let z = Math.random() * 400 - 10;
+
+        cardMesh.position.set(x, y, z); //카드 위치 지정
+        cardMesh.rotation.set(Math.random() * 30, 0, Math.random() * 30); //카드의 회전 지정
+
+        this.scene.add(cardMesh);
+        this.loadingImage++;
+      });
+    },
+
+    addLight(){
+      const color = 0xffffff;
+      const intensity = 0.4;
+      const light = new THREE.PointLight(color, intensity);
+      light.castShadow = true;
+
+      // addLight(15, 15, 20);
+      // ...pos로 위치값을 바로 가져옵니다.
+      //light.position.set(...pos);
+
+      light.shadow.mapSize.width = 512; // default
+      light.shadow.mapSize.height = 512; // default
+      light.shadow.camera.near = 0.5; // default
+      light.shadow.camera.far = 500; // default
+      light.shadow.radius = 5;
+      light.shadow.blurSamples = 5;
+      this.scene.add(light);
+
+      // const helper = new THREE.PointLightHelper(light);
+      // scene.add(helper);
+    },
+
+    animate(){
+      //this.controls.update();
+      //this.camera.lookAt(this.scene.position);
+
+      if(this.totalNum <= this.loadingImage){
+
+        if(this.cameraZ > 2){
+          this.cameraZ = this.cameraZ - 0.7;
+        }
+        if(this.cameraY > 2.5){
+          this.cameraY = this.cameraY - 0.15;
+        }
+        this.camera.position.z =  this.cameraZ;
+        this.camera.position.y =  this.cameraY;
+      }
+
+
+      this.camera.updateProjectionMatrix();
+      this.renderer.render(this.scene, this.camera);
+      requestAnimationFrame(this.animate);
+    }
+  }
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
